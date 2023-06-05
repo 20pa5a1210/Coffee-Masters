3ca026d78b0e2bc98312464e89ee31b25f0d3f32
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-06-05 12:40:46 +0530

    Feat: Router poptstate and params

--- services/Router.js
+++ services/Router.js
@@ -1,16 +1,49 @@
 const Router = {
     init: () => {
         document.querySelectorAll("a.navlink").forEach(a => {
-            a.addEventListener("click",event => {
+            a.addEventListener("click", event => {
                 event.preventDefault();
                 const url = event.target.getAttribute("href")
                 Router.go(url);
             })
         })
+        window.addEventListener("popstate",event => {
+            Router.go(event.state.route,false);
+        })
+        Router.go(location.pathname);
     },
 
     go: (route, addToHistory = true) => {
         console.log(`Going to ${route}`)
+
+        if (addToHistory) {
+            history.pushState({ route }, "", route);
+        }
+        let pageElement = null;
+        switch (route) {
+            case "/":
+                pageElement = document.createElement("h1");
+                pageElement.textContent = "Menu";
+                break;
+            case "/order":
+                pageElement = document.createElement("h1");
+                pageElement.textContent = "Order";
+                break;
+            default:
+                if (route.startsWith("/product-")) {
+                    pageElement = document.createElement("h1");
+                    pageElement.textContent = "Order";
+                    const paramId = route.substring(route.lastIndexOf("-") + 1);
+                    pageElement.dataset.id = paramId;
+                }
+        }
+        if (pageElement) {
+            const cache = document.querySelector('main')
+            cache.innerHTML = "";
+            cache.appendChild(pageElement)
+            window.scrollX = 0;
+            window.scrollY = 0;
+        }
     }
 }
 
